778e0f190b0c07543e1f38f1737b60ff799243b7
himrock922 <himrock922@gmail.com>
2020-02-22 19:25:09 +0900

    PostgreSQLの設定追加

--- config/travis.exs
+++ config/travis.exs
@@ -11,7 +11,7 @@ config :daily_report, DailyReport.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :daily_report, DailyReportWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 5432],
   server: false
 
 # Print only warnings and errors during test
