e9c06ed81f1d30cd7b50110c6f9eedb5068da3aa
himrock922 <himrock922@gmail.com>
2020-02-24 22:08:06 +0900

    ファイル追加

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,8 @@
+[
+    # {short_description}
+    {":0:unknown_function Function :erl_types.t_is_opaque/1/1 does not exist."},
+    # {short_description, warning_type}
+    {":0:unknown_function Function :erl_types.t_to_string/1 does not exist.", :unknown_function},
+    # {short_description, warning_type, line}
+    {":0:unknown_function Function :erl_types.t_to_string/1 does not exist.", :unknown_function, 0}
+  ]
