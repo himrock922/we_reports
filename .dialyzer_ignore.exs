c7e34f658e546bd48f4808253630c833ebbaecb8
himrock922 <himrock922@gmail.com>
2020-02-24 22:11:32 +0900

    ファイル追加

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,8 +1,4 @@
 [
-    # {short_description}
-    {":0:unknown_function Function :erl_types.t_is_opaque/1/1 does not exist."},
-    # {short_description, warning_type}
-    {":0:unknown_function Function :erl_types.t_to_string/1 does not exist.", :unknown_function},
-    # {short_description, warning_type, line}
-    {":0:unknown_function Function :erl_types.t_to_string/1 does not exist.", :unknown_function, 0}
+    {":0:unknown_function Function ExUnit.CaseTemplate.__proxy__/2 does not exist."},
+    {":0:unknown_function Function ExUnit.Callbacks.__merge__/3 does not exist."}
   ]
