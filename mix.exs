25485360d66bc55886acec1a060e0e4f6d8a734b
himrock922 <himrock922@gmail.com>
2020-02-24 21:14:34 +0900

    rename

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule DailyReport.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [ignore_warnings: ".dialyzer_ignore.exs"],
     ]
   end
 
