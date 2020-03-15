d814c192be94454429fbbf4fdc3be02a73bd777b
himrock922 <himrock922@gmail.com>
2020-03-15 16:08:10 +0900

    phoenix_html import

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,4 @@ import 'fuse.js/dist/fuse';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap-select-dropdown/dist/bootstrap-select-dropdown.min';
+import "phoenix_html"
