8390ba8e03b16404a0d21f98681fd56a3578cbd2
himrock922 <himrock922@gmail.com>
2020-03-21 15:06:19 +0900

    html修正

--- lib/we_reports_web/controllers/group_controller.ex
+++ lib/we_reports_web/controllers/group_controller.ex
@@ -11,8 +11,8 @@ defmodule WeReportsWeb.GroupController do
   end
 
   def new(conn, _params) do
-    changeset = Groups.change_group(%Group{})
     users = get_users()
+    changeset = Groups.change_group(%Group{})
     render(conn, "new.html", changeset: changeset, users: users)
   end
 
@@ -21,7 +21,7 @@ defmodule WeReportsWeb.GroupController do
     case Groups.create_group(group_params) do
       {:ok, group} ->
         conn
-        |> put_flash(:info, "Group created successfully.")
+        |> put_flash(:success, "グループの作成に成功しました。")
         |> redirect(to: Routes.group_path(conn, :show, group))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset, users: users)
@@ -34,9 +34,9 @@ defmodule WeReportsWeb.GroupController do
   end
 
   def edit(conn, %{"id" => id}) do
+    users = get_users()
     group = Groups.get_group!(id)
     changeset = Groups.change_group(group)
-    users = get_users()
     render(conn, "edit.html", group: group, changeset: changeset, users: users)
   end
 
@@ -47,7 +47,7 @@ defmodule WeReportsWeb.GroupController do
     case Groups.update_group(group, group_params) do
       {:ok, group} ->
         conn
-        |> put_flash(:info, "Group updated successfully.")
+        |> put_flash(:success, "グループの更新に成功しました。")
         |> redirect(to: Routes.group_path(conn, :show, group))
 
       {:error, %Ecto.Changeset{} = changeset} ->
@@ -60,7 +60,7 @@ defmodule WeReportsWeb.GroupController do
     {:ok, _group} = Groups.delete_group(group)
 
     conn
-    |> put_flash(:info, "Group deleted successfully.")
+    |> put_flash(:success, "グループの削除に成功しました。")
     |> redirect(to: Routes.group_path(conn, :index))
   end
 
