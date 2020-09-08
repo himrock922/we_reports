5b53d2a2701894e74e27f321b4a3fe491f237d8d
himrock922 <himrock922@gmail.com>
2020-09-08 23:11:06 +0900

    レビュー修正

--- lib/we_reports_web/live/daily_reports/daily_report_form_live.ex
+++ lib/we_reports_web/live/daily_reports/daily_report_form_live.ex
@@ -18,10 +18,6 @@ defmodule WeReportsWeb.DailyReportFormLive do
           user: user
         ]
         {:ok, assign(socket, assigns)}
-      {:error, _} ->
-        {:noreply,
-          socket
-          |> put_flash(:error, "ユーザが見つかりませんでした")}
     end
   end
 
--- test/we_reports_web/controllers/daily_report_controller_test.exs
+++ test/we_reports_web/controllers/daily_report_controller_test.exs
@@ -1,77 +1,52 @@
 defmodule WeReportsWeb.DailyReportControllerTest do
   use WeReportsWeb.ConnCase
 
-  alias WeReports.DailyReports
+  alias WeReports.{DailyReports, UserManager}
 
   @create_attrs %{next_action: "some next_action", summary: "some summary"}
-  @update_attrs %{next_action: "some updated next_action", summary: "some updated summary"}
-  @invalid_attrs %{next_action: nil, summary: nil}
+  @create_user %{password: "some password", username: "some username"}
 
   def fixture(:daily_report) do
     {:ok, daily_report} = DailyReports.create_daily_report(@create_attrs)
     daily_report
   end
 
-  describe "index" do
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(@create_user)
+      |> UserManager.create_user()
+    user
+  end
+
+  describe "日報一覧" do
+    @tag :authenticated
     test "lists all daily_reports", %{conn: conn} do
       conn = get(conn, Routes.daily_report_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Daily reports"
+      assert html_response(conn, 200) =~ "日報一覧"
     end
   end
 
-  describe "new daily_report" do
+  describe "日報新規作成" do
+    @tag :authenticated
     test "renders form", %{conn: conn} do
       conn = get(conn, Routes.daily_report_path(conn, :new))
-      assert html_response(conn, 200) =~ "New Daily report"
+      assert html_response(conn, 200) =~ "新規日報作成"
     end
   end
 
-  describe "create daily_report" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.daily_report_path(conn, :create), daily_report: @create_attrs)
-
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.daily_report_path(conn, :show, id)
-
-      conn = get(conn, Routes.daily_report_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show Daily report"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.daily_report_path(conn, :create), daily_report: @invalid_attrs)
-      assert html_response(conn, 200) =~ "New Daily report"
-    end
-  end
-
-  describe "edit daily_report" do
+  describe "日報詳細" do
     setup [:create_daily_report]
-
-    test "renders form for editing chosen daily_report", %{conn: conn, daily_report: daily_report} do
-      conn = get(conn, Routes.daily_report_path(conn, :edit, daily_report))
-      assert html_response(conn, 200) =~ "Edit Daily report"
-    end
-  end
-
-  describe "update daily_report" do
-    setup [:create_daily_report]
-
-    test "redirects when data is valid", %{conn: conn, daily_report: daily_report} do
-      conn = put(conn, Routes.daily_report_path(conn, :update, daily_report), daily_report: @update_attrs)
-      assert redirected_to(conn) == Routes.daily_report_path(conn, :show, daily_report)
-
+    @tag :authenticated
+    test "renders form", %{conn: conn, daily_report: daily_report} do
       conn = get(conn, Routes.daily_report_path(conn, :show, daily_report))
-      assert html_response(conn, 200) =~ "some updated next_action"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, daily_report: daily_report} do
-      conn = put(conn, Routes.daily_report_path(conn, :update, daily_report), daily_report: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Edit Daily report"
+      assert html_response(conn, 200) =~ "some summary"
     end
   end
 
-  describe "delete daily_report" do
+  describe "日報削除" do
     setup [:create_daily_report]
-
+    @tag :authenticated
     test "deletes chosen daily_report", %{conn: conn, daily_report: daily_report} do
       conn = delete(conn, Routes.daily_report_path(conn, :delete, daily_report))
       assert redirected_to(conn) == Routes.daily_report_path(conn, :index)
@@ -82,7 +57,9 @@ defmodule WeReportsWeb.DailyReportControllerTest do
   end
 
   defp create_daily_report(_) do
-    daily_report = fixture(:daily_report)
+    user = user_fixture()
+    add_user_create_attrs = Map.merge(@create_attrs, %{user_id: user.id})
+    {:ok, daily_report} = DailyReports.create_daily_report(add_user_create_attrs)
     {:ok, daily_report: daily_report}
   end
 end
